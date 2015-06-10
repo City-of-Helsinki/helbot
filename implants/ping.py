93501665cd73678f16aea8c3c8f3779692ad3ea0
Juha Yrjölä <juha.yrjola@iki.fi>
2015-06-10 19:36:17 +0300

    Fix crash with edit messages

--- implants/ping.py
+++ implants/ping.py
@@ -6,20 +6,24 @@ class PingImplant(bot.BotImplant):
 
     @asyncio.coroutine
     def handle_message(self, msg):
+        if 'user' not in msg:
+            return False
         if msg['user'] == self.rtm.user_id:
-            return
+            return False
         user = self.rtm.find_user(msg['user'])
         if msg['channel'][0] == 'D':
             # Direct message
             if msg['text'].strip().lower() == 'ping':
                 yield from user.send_im('pong')
+                return True
         else:
             # Mentions me?
             mention_text = '<@{}>'.format(self.rtm.user_id)
             if not msg['text'].startswith(mention_text):
-                return
+                return False
             text = msg['text'].replace(mention_text, '', 1)
             if text.strip().lower() == 'jou':
                 reply = {'text': '<@{}> jou jou'.format(msg['user']),
                          'type': 'message', 'channel': msg['channel']}
                 yield from self.rtm.send_event(reply)
+                return True
